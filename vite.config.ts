c56ba748b7926d4ea0f24f4c5e39bda4fba4280c
danilkunashev <dan_kunashev@mail.ru>
2026-05-27 00:04:02 +0300

    ее

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/invite/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
